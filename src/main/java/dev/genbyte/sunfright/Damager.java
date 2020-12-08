cebe09b53f4504d38dafd8420c53b5bacfaf36b8
Brad Alfirevic <brad@genbyte.dev>
2020-12-07 21:01:55 -0600

    Get working on 1.12.2

--- src/main/java/dev/genbyte/sunfright/Damager.java
+++ src/main/java/dev/genbyte/sunfright/Damager.java
@@ -1,8 +1,12 @@
 package dev.genbyte.sunfright;
 
 import java.util.Collection;
+import java.util.Optional;
 import java.util.Random;
+import java.util.logging.Level;
 
+import org.bukkit.Color;
+import org.bukkit.DyeColor;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.World;
@@ -10,8 +14,8 @@ import org.bukkit.block.Block;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.Damageable;
-import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.material.Dye;
+import org.bukkit.material.MaterialData;
 import org.bukkit.scheduler.BukkitRunnable;
 
 public class Damager extends BukkitRunnable {
@@ -37,28 +41,48 @@ public class Damager extends BukkitRunnable {
 			int y = loc.getBlockY();
 			int z = loc.getBlockZ();
 			// Returns one lower than it should?
-			int highestY = sunnedWorld.getHighestBlockAt(loc).getLocation().getBlockY()+1;
+			Optional<Block> highestBlock = actualHighestBlock(sunnedWorld, loc);
 
-			for (int i = y; i < highestY; ++i) {
-				Block current = sunnedWorld.getBlockAt(x, i, z);
+			if (highestBlock.isPresent()) {
+				int highestY = highestBlock.get().getY();
 
-				if (!blockShouldDamage(current.getType())) {
-					/* player rulled to be safe. Remove their helmet if it's the one we gave, but
-					   only do so if the skylight is less than three. This will keep us from
-					   removing the starter helmet if they're just chopping down a tree */
-					if (player.getInventory().getHelmet() != null &&
-					    player.getLocation().getBlock().getLightFromSky() > 3 &&
-					    player.getInventory().getHelmet().getEnchantmentLevel(Enchantment.VANISHING_CURSE) == 2)
-				   	{
-					   player.getInventory().setHelmet(new ItemStack(Material.AIR));
-				   	}
+				for (int i = y; i <= highestY; ++i) {
+					Block current = sunnedWorld.getBlockAt(x, i, z);
 
-					return;
+					if (!blockShouldDamage(current)) {
+						/* player rulled to be safe. Remove their helmet if it's the one we gave, but
+						only do so if the skylight is less than three. This will keep us from
+						removing the starter helmet if they're just chopping down a tree */
+						if (player.getInventory().getHelmet() != null &&
+							player.getLocation().getBlock().getLightFromSky() < 3 &&
+							player.getInventory().getHelmet().getEnchantmentLevel(Enchantment.VANISHING_CURSE) == 2)
+						{
+							player.getInventory().setHelmet(new ItemStack(Material.AIR));
+						}
+
+						return;
+					}
 				}
 			}
 
 			new DoDamage(player, sf.damagaPerSecond).runTask(sf);
 		});
+	}
+
+	// 1.12.2 getHighetBlockAt method only returns the highest non-transparent.
+	// Glass is transparent so I guess we have to do this ourselves
+	private Optional<Block> actualHighestBlock(World world, Location location) {
+		int lowerBound = world.getHighestBlockYAt(location);
+		int upperBound = world.getMaxHeight();
+
+		for (int i = upperBound; i > lowerBound; --i) {
+			Block block = world.getBlockAt(location.getBlockX(), i, location.getBlockZ());
+			if (!block.isEmpty()) {
+				return Optional.of(block);
+			}
+		}
+
+		return Optional.empty();
 	}
 
 	private boolean timeShouldDamage() {
@@ -92,14 +116,17 @@ public class Damager extends BukkitRunnable {
 	things we check.
 	*/
 	@SuppressWarnings("deprecation")
-	private boolean blockShouldDamage(Material mat) {
-		String key = mat.getKey().getKey().toLowerCase();
-		
-		if (mat == Material.BLACK_STAINED_GLASS) {
+	private boolean blockShouldDamage(Block block) {
+		Material mat = block.getType();
+		MaterialData matData = block.getState().getData();
+		String key = mat.toString().toLowerCase();
+
+		// 15 is the magic value for black
+		if (mat == Material.STAINED_GLASS && matData.getData() == 15) {
 			return false;
 		}
 
-		return mat.isTransparent() || key.indexOf("glass") != -1 || key.indexOf("leaves") != -1 || key.indexOf("sign") != -1 || key.indexOf("trapdoor") != -1 || key.indexOf("fence") != -1 || key.indexOf("bed") != -1 || mat == Material.ICE || mat == Material.HOPPER || mat == Material.COBWEB;
+		return mat.isTransparent() || key.indexOf("glass") != -1 || key.indexOf("leaves") != -1 || key.indexOf("sign") != -1 || key.indexOf("trapdoor") != -1 || key.indexOf("fence") != -1 || key.indexOf("bed") != -1 || mat == Material.ICE || mat == Material.HOPPER || mat == Material.WEB;
 	}
 
 	private class DoDamage extends BukkitRunnable {
@@ -115,43 +142,36 @@ public class Damager extends BukkitRunnable {
 			ItemStack helmet = player.getInventory().getHelmet();
 
 			if (helmet != null) {
-				ItemMeta helmetMeta = helmet.getItemMeta();
+				short helmetDamage = helmet.getDurability();
+				int fireProtLevel = helmet.getEnchantmentLevel(Enchantment.PROTECTION_FIRE);
 
-				if (helmetMeta instanceof Damageable) {
-					Damageable helmetDamageable = (Damageable) helmetMeta;
-					int helmetDamage = helmetDamageable.getDamage();
-					int fireProtLevel = helmet.getEnchantmentLevel(Enchantment.PROTECTION_FIRE);
+				if (fireProtLevel < 1) {
+					damagePlayer();
+					return;
+				}
 
-					if (fireProtLevel < 1) {
-						damagePlayer();
-						return;
+				if (helmetDamage + damage >= helmet.getType().getMaxDurability()) {
+					if (helmet.getEnchantmentLevel(Enchantment.VANISHING_CURSE) == 2) {
+						int bindLevel = helmet.getEnchantmentLevel(Enchantment.BINDING_CURSE);
+
+						if (bindLevel < 5) {
+							helmet.setDurability((short) 0);
+							helmet.addUnsafeEnchantment(Enchantment.BINDING_CURSE, bindLevel + 1);
+
+							return;
+						}
 					}
 
-					if (helmetDamage + damage >= helmet.getType().getMaxDurability()) {
-						if (helmet.getEnchantmentLevel(Enchantment.VANISHING_CURSE) == 2) {
-							int bindLevel = helmet.getEnchantmentLevel(Enchantment.BINDING_CURSE);
-
-							if (bindLevel < 5) {
-								helmetDamageable.setDamage(0);
-								helmet.setItemMeta((ItemMeta) helmetDamageable);
-								helmet.addUnsafeEnchantment(Enchantment.BINDING_CURSE, bindLevel + 1);
-
-								return;
-							}
-						}
-
-						player.getInventory().setHelmet(new ItemStack(Material.AIR));
-					} else {
-						// Formula from https://minecraft.gamepedia.com/Unbreaking
-						// Origintal is 60 + (40 / (level+1)) but we subtract one from fireProtLevel
-						// so the +1 cancels
-						int chanceToDamage = 60 + (40 / (fireProtLevel));
-						
-						if (rand.nextInt(99)+1 <= chanceToDamage) {
-							int calculatedDamage = (int) Math.ceil(damage / 2);
-							helmetDamageable.setDamage(helmetDamage + calculatedDamage);
-							helmet.setItemMeta((ItemMeta) helmetDamageable);
-						}
+					player.getInventory().setHelmet(new ItemStack(Material.AIR));
+				} else {
+					// Formula from https://minecraft.gamepedia.com/Unbreaking
+					// Origintal is 60 + (40 / (level+1)) but we subtract one from fireProtLevel
+					// so the +1 cancels
+					int chanceToDamage = 60 + (40 / (fireProtLevel));
+					
+					if (rand.nextInt(99)+1 <= chanceToDamage) {
+						int calculatedDamage = (int) Math.ceil(damage / 2);
+						helmet.setDurability((short) (helmetDamage + calculatedDamage));
 					}
 				}
 			} else {
